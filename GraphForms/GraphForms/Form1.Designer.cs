74f0e947e73b4cb05344febcd60357368bf07613
Liaihua <ilyavovchenko431@gmail.com>
2019-04-28 22:16:56 +1000

    Add files via upload
    Please note that my project also named "GraphForms". I tried to get experience in working with WindowsForms.Chart component.

---
+++ GraphForms/GraphForms/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace SystemChartMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.CPUchart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.RAMchart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.CPUchart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RAMchart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CPUchart
+            // 
+            this.CPUchart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            chartArea1.Name = "ChartArea1";
+            this.CPUchart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.CPUchart.Legends.Add(legend1);
+            this.CPUchart.Location = new System.Drawing.Point(12, 12);
+            this.CPUchart.Name = "CPUchart";
+            series1.BackImageTransparentColor = System.Drawing.Color.White;
+            series1.BackSecondaryColor = System.Drawing.Color.White;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Navy;
+            series1.Legend = "Legend1";
+            series1.Name = "CPUSeries";
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.CPUchart.Series.Add(series1);
+            this.CPUchart.Size = new System.Drawing.Size(678, 557);
+            this.CPUchart.TabIndex = 0;
+            this.CPUchart.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tag = "ShowCPUUsage";
+            this.timer1.Tick += new System.EventHandler(this.ShowCPUUsage);
+            // 
+            // RAMchart
+            // 
+            this.RAMchart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.Name = "ChartArea1";
+            this.RAMchart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.RAMchart.Legends.Add(legend2);
+            this.RAMchart.Location = new System.Drawing.Point(696, 12);
+            this.RAMchart.Name = "RAMchart";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "RAMTotal";
+            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.RAMchart.Series.Add(series2);
+            this.RAMchart.Size = new System.Drawing.Size(662, 557);
+            this.RAMchart.TabIndex = 1;
+            this.RAMchart.Tag = "";
+            this.RAMchart.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 581);
+            this.Controls.Add(this.RAMchart);
+            this.Controls.Add(this.CPUchart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CPUchart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RAMchart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart CPUchart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart RAMchart;
+    }
+}
+
---
+++ GraphForms/GraphForms/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SystemChartMonitor
+{
+    public partial class Form1 : Form
+    {
+        readonly PerformanceCounter[] CPUCounterDict = new PerformanceCounter[Environment.ProcessorCount];
+        readonly PerformanceCounter RAMCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+        public Form1()
+        {
+            InitializeComponent();
+            
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Инициализация объектов Chart
+            for(int proc = 0; proc < Environment.ProcessorCount; proc++)
+            {
+                PerformanceCounter proccounter = new PerformanceCounter("Processor", "% Processor Time", proc.ToString());
+                CPUCounterDict[proc] = proccounter;
+                Series cpuseries = new Series("CPU" + proc, 100);
+                cpuseries.ChartType = SeriesChartType.Spline;
+                CPUchart.Series.Add(cpuseries);
+            }
+            
+        }
+
+        private void ShowCPUUsage(object sender, EventArgs e)
+        {
+            for (int proc = 0; proc < Environment.ProcessorCount; proc++)
+            {
+                CPUchart.Series[proc].Points.AddY(CPUCounterDict[proc].NextValue());
+
+               
+            }
+            RAMchart.Series[0].Points.AddY(RAMCounter.NextValue());
+            
+        }
+    }
+}
